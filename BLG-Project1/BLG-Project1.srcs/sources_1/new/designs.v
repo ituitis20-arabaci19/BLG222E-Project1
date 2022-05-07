e0031a0053b1f82890142d0edabc4d92e11f9764
ituitis20-toy19 <toy.cemalettin.celal@gmail.com>
2022-05-07 22:54:46 +0300

    memory add

--- BLG-Project1/BLG-Project1.srcs/sources_1/new/designs.v
+++ BLG-Project1/BLG-Project1.srcs/sources_1/new/designs.v
@@ -436,5 +436,30 @@ module PART3(A,B,FunSel,OutALU,OutFlag,CLK);
     MUX16_4_8bit alu(A,B,~A,~B,adder_result,adder_result,adder_result,(A&B),(A|B),((A&~B)|(~A&B)),shift,shift,shift,shift,shift,shift,OutALU,FunSel);
 
 endmodule
+
+module Memory(
+    input wire[7:0] address,
+    input wire[7:0] data,
+    input wire wr, //Read = 0, Write = 1
+    input wire cs, //Chip is enable when cs = 0
+    input wire clock,
+    output reg[7:0] o // Output
+);
+    //Declaration oýf the RAM Area
+    reg[7:0] RAM_DATA[0:255];
+    //Read Ram data from the file
+    initial $readmemh("RAM.mem", RAM_DATA);
+    //Read the selected data from RAM
+    always @(*) begin
+        o = ~wr && ~cs ? RAM_DATA[address] : 8'hZ;
+    end
+    
+    //Write the data to RAM
+    always @(posedge clock) begin
+        if (wr && ~cs) begin
+            RAM_DATA[address] <= data; 
+        end
+    end
+endmodule
         
 
